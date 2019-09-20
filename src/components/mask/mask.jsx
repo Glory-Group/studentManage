95fa0e4f0d87b77e49e49ec568f4f5cafd6af006
gxb <214056383@qq.com>
2019-09-20 20:40:13 +0800

    score

--- src/components/mask/mask.jsx
+++ src/components/mask/mask.jsx
@@ -1,26 +1,26 @@
 import React from "react"
 import "./scss/style.css"
 const Mask = props => {
-    let { title, visible, onOk, onCancel } = props
-    return (<div>
-        {visible && <div className="mask-wrap">
-            <div className="mask-container">
-                <div className="mask-title">
-                    {title}
-                </div>
-                <div className="mask-content">
-                    {props.children}
+	let { title, visible, onOk, onCancel } = props
+	return (<div>
+		{visible && <div className="mask-wrap">
+			<div className="mask-container">
+				<div className="mask-title">
+					{title}
+				</div>
+				<div className="mask-content">
+					{props.children}
 
-                </div>
-                <div className="mask-btns">
-                    <span className="btn cancel-btn" onClick={onCancel}>取消</span>
-                    <span className="btn sure-btn" onClick={onOk}>确认</span>
-                </div>
-            </div>
-        </div>}
+				</div>
+				<div className="mask-btns">
+					<span className="btn cancel-btn" onClick={onCancel}>取消</span>
+					<span className="btn sure-btn" onClick={onOk}>确认</span>
+				</div>
+			</div>
+		</div>}
 
-    </div>
+	</div>
 
-    )
+	)
 }
 export default Mask
